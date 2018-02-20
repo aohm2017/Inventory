f2289594f86f178b31c447bde66e3fe192c87ccb
Alyssa <35383433+aohm2017@users.noreply.github.com>
2018-02-19 19:33:16 -0500

    Updated makeupcategory table
    sub category was lost due to device replacement so I needed to update the table

---
+++ DropAndCreate dbo.MakeupCategory.sql
@@ -0,0 +1,22 @@
+﻿USE [Inventory]
+GO
+
+/****** Object: Table [dbo].[MakeupCategory] Script Date: 2/19/2018 6:39:53 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[MakeupCategory];
+
+
+GO
+CREATE TABLE [dbo].[MakeupCategory] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [Category]    VARCHAR (50)     NULL,
+	[SubCategory] VARCHAR(50) NULL,
+    [Description] VARCHAR (250)    NULL
+);
+
+
